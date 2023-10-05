00c25f693b222582ebd2dbbc1b85711ccdf89255
Pablo Castelo <pablohenriquec7@gmail.com>
2023-10-05 10:35:56 -0300

    Feat: Removing swiper

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,23 +1,18 @@
-import { Swiper, SwiperSlide } from "swiper/react";
-import 'swiper/css';
+
 import { ProjectCard } from "../ProjectCard";
 
-interface CarouselProps {
-  
-}
+
+interface CarouselProps {}
+
+
 
 export const Carousel: React.FC<CarouselProps> = () => {
-  return (
-    <>
-      {/* <Swiper slidesPerView={3}>
-        <SwiperSlide>TESTE1</SwiperSlide>
-        <SwiperSlide>TESTE2</SwiperSlide>
-        <SwiperSlide>TESTE3</SwiperSlide>
-        <SwiperSlide>TESTE4</SwiperSlide>
-        <SwiperSlide>TESTE5</SwiperSlide>
-        <SwiperSlide>TESTE6</SwiperSlide>
-      </Swiper> */}
-      <ProjectCard />
-    </>
-  );
+
+
+
+	return (
+		<>
+			
+		</>
+	);
 };
