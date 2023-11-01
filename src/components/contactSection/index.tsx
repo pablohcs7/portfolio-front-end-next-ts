67e9f9066c602530c8d3989d6463a2fa85260051
Pablo Castelo <pablohenriquec7@gmail.com>
2023-11-01 19:24:30 -0300

    Feat: Continuing the development of the contact section and testing create a custom icon with chakra

--- src/components/contactSection/index.tsx
+++ src/components/contactSection/index.tsx
@@ -1,17 +1,34 @@
-import { Flex } from "@chakra-ui/react";
+import { Box, Flex, Text } from "@chakra-ui/react";
 import { MenuHeaderItem } from "../MenuHeaderItem";
+import ChakraNextImage from "../ChakraNextImage";
+import { DiscordIcon } from "../IconsComponents/DiscordIcon";
 
 interface ContactSectionProps {}
 
 export const ContactSection: React.FC<ContactSectionProps> = () => {
 	return (
 		<>
-			<Flex flexDirection={"column"} mt={'5rem'}>
+			<Flex flexDirection={"column"} mt={"5rem"}>
 				<MenuHeaderItem
 					optionLink="/contact"
 					optionName="Entre em contato"
 					customColor="white"
 				/>
+				<Text mt={"2rem"}>
+					Estou em busca de oportunidades full time. Mas, se tiver alguma
+					proposta ou dúvida, sinta-se à vontade para entrar em contato comigo.
+				</Text>
+				<Box>
+					<Flex alignItems={"center"} mt={"1rem"}>
+						<DiscordIcon
+							color={"secondary"}
+							boxSize={"2.5rem"}
+							_hover={{ color: "primary", transition: "0.5s", cursor: 'pointer' }}
+						/>
+						<Text color={"secondary"}>pablohcs7</Text>
+					</Flex>
+					<Box></Box>
+				</Box>
 			</Flex>
 		</>
 	);
